288ccfce73b6807080b6145cd6128788d9ed28bc
Matthew Morais <moraismatthew@yahoo.com>
2017-09-18 19:32:15 -0400

    Axios Installed Axios was installed to prepare for API use on the server.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
+var axios = require('axios');
 
 server.use(express.static(__dirname + '/public'));
 server.get('/', function(req, res){
