83337c1d1c83f12c4579c35ace483882e0b2bba4
Matthew Morais <moraismatthew@yahoo.com>
2017-09-19 01:50:52 -0400

    App Officially Works

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,39 @@
 var app = new Vue({
     el: "#app",
     data: {
-        message: 'Hello Vue!'
+        message: null,
+        next: null,
+        previous: null
+    },
+    methods: {
+        nextID: function() {
+            axios.get(`/verse/${this.next}`)
+                .then(function(results){
+                    app.next = results.data.response.verses[0].next.verse.id;
+                    app.previous = results.data.response.verses[0].previous.verse.id;
+                    app.message = results.data.response.verses[0].text;
+                })
+                .catch(function(err){
+                }) 
+        },
+        previousID: function() {
+            axios.get(`/verse/${this.previous}`)
+                .then(function(results){
+                    app.next = results.data.response.verses[0].next.verse.id;
+                    app.previous = results.data.response.verses[0].previous.verse.id;
+                    app.message = results.data.response.verses[0].text;
+                })
+                .catch(function(err){
+                }) 
+        }
     }
 })
+
+axios.get('/random/verse/')
+    .then(function(results){
+        app.next = results.data.response.verses[0].next.verse.id;
+        app.previous = results.data.response.verses[0].previous.verse.id;
+        app.message = results.data.response.verses[0].text;
+    })
+    .catch(function(err){
+    })
